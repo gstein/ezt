eba37d49afe56e27f8aba6e6faccfffc20c3ceb1
gstein <gstein@319997a1-2d1a-0410-b116-a13720468ddf>
2008-07-01 05:12:37 +0000

    Log message copied from svn.webdav.org/repos/projects:
    > r151 | gstein | 2003-12-12 01:23:37 -0800 (Fri, 12 Dec 2003) | 23 lines
    > 
    > The old code assumed the leadings parts of a ref had to be sequences if
    > trailing parts were to be sequences, too. However, this failed for a
    > sequence which was stored in an object. nonseq.seq would throw an error.
    > 
    > The new code will now look at the reference and see if some part of it
    > refers to an active [for] reference. If so, then it returns the
    > appropriate construction for later reference to the iterative value.
    > 
    > The problem identification and an initial patch were provided by Sander
    > Striker <striker@apache.org>, then evolved by gstein.
    > 
    > * ezt.py:
    >   (_prepare_ref): look for a [for] reference with any part of the
    >     reference, rather than assuming all 0..n parts are sequences.
    > 
    > * tests/test.py: a simple test to check this case
    > * tests/test.ezt: a template for test.py
    > 
    > * tests/timing.py: short dev script to try different algorithms for
    >     determining if a reference contains an active [for] identifier.
    > 
    > * properties on '.': set svn:ignore to ignore *.pyc

---
+++ tests/test.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+
+import sys
+
+sys.path.insert(0, '..')
+import ezt
+
+class _item:
+  pass
+
+t = ezt.Template('test.ezt')
+
+nonseq = _item()
+nonseq.attr = 'nonseq.attr'
+
+seqitem = _item()
+seqitem.attr = 'seqitem.attr'
+nonseq.seq = [ seqitem, seqitem ]
+
+data = { 'nonseq' : nonseq }
+
+t.generate(sys.stdout, data)

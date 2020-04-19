7d67ccdd365df3dfcd3128d9779f5c349a7f146e
Greg Stein <gstein@gmail.com>
2020-04-19 12:07:43 -0500

    Get the unit tests running under Python 3.x.
    NOTE: two tests fail, by design.

--- tests/ezt_test.py
+++ tests/ezt_test.py
@@ -7,7 +7,12 @@
 
 import sys
 import unittest
-import StringIO
+try:
+  from StringIO import StringIO
+  PY3 = False
+except:
+  from io import StringIO
+  PY3 = True
 
 sys.path.insert(0, '..')
 import ezt
@@ -15,34 +20,40 @@ import ezt
 
 class EztUnitTest(unittest.TestCase):
   def _runTemplate(self, template, data, fmt=ezt.FORMAT_RAW):
+    # parse() takes a str, not bytes. assume utf-8.
+    if PY3 and isinstance(template, bytes):
+      template = template.decode('utf-8')
     t = ezt.Template()
     t.parse(template, base_format=fmt)
-    o = StringIO.StringIO()
+    o = StringIO()
     t.generate(o, data)
     return o.getvalue()
 
   def _runTemplateFile(self, path, data):
     t = ezt.Template('ezt_test_data/' + path)
-    o = StringIO.StringIO()
+    o = StringIO()
     t.generate(o, data)
     return o.getvalue()
 
   def testSimpleReplacement(self):
     d = self._runTemplate('this is a [X].', {'X': 'test'})
-    self.assertEquals('this is a test.', d)
+    self.assertEqual('this is a test.', d)
 
   def testSimpleReplacementUtf8Encoded(self):
+    ### fails because _runTemplate changes the bytes back to
+    ### a unicode string so that _parse() can .split() it.
+
     # If all inputs are byte strings encoded with an encoding that is a superset
     # of ASCII (e.g. UTF-8), the output will be a byte string with the same
     # encoding. This mode of operation may not be supported in future ezt
     # versions running on Python 3.
     t = u'◄ [X] ►'.encode('utf-8')
     d = self._runTemplate(t, {'X': u'♥'.encode('utf-8')})
-    self.assertEquals(u'◄ ♥ ►'.encode('utf-8'), d)
+    self.assertEqual(u'◄ ♥ ►'.encode('utf-8'), d)
 
   def testSimpleReplacementUnicode(self):
     d = self._runTemplate(u'◄ [X] ►', {'X': u'♥'})
-    self.assertEquals(u'◄ ♥ ►', d)
+    self.assertEqual(u'◄ ♥ ►', d)
 
   def testSimpleReplacementStrTemplateAndUnicodeVariable(self):
     # Mixing str and unicode objects is allowed as long as the str objects only
@@ -50,25 +61,27 @@ class EztUnitTest(unittest.TestCase):
     # Python 3 this test will become redundant and can be deleted (it will be
     # equivalent to testSimpleReplacementUnicode).
     d = self._runTemplate('I [verb] Python', {'verb': u'♥'})
-    self.assertEquals(u'I ♥ Python', d)
+    self.assertEqual(u'I ♥ Python', d)
 
   def testSimpleReplacementUnicodeTemplateAndStrVariable(self):
     # Just like testSimpleReplacementStrTemplateAndUnicodeVariable above, this
     # test can be safely deleted after the switch to Python 3.
     d = self._runTemplate(u'I ♥ [language]', {'language': 'Python'})
-    self.assertEquals(u'I ♥ Python', d)
+    self.assertEqual(u'I ♥ Python', d)
 
   def testLiteral(self):
     d = self._runTemplate('this is a ["trivial test"].', {})
-    self.assertEquals('this is a trivial test.', d)
+    self.assertEqual('this is a trivial test.', d)
 
   def testLiteralUtf8Encoded(self):
+    ### fails because _runTemplate changes the bytes back to
+    ### a unicode string so that _parse() can .split() it.
     d = self._runTemplate(u'◄ ["♥"] ►'.encode('utf-8'), {})
-    self.assertEquals(u'◄ ♥ ►'.encode('utf-8'), d)
+    self.assertEqual(u'◄ ♥ ►'.encode('utf-8'), d)
 
   def testLiteralUnicode(self):
     d = self._runTemplate(u'◄ ["♥"] ►', {})
-    self.assertEquals(u'◄ ♥ ►', d)
+    self.assertEqual(u'◄ ♥ ►', d)
 
   def testAttributes(self):
     class _BlahBlah:
@@ -77,12 +90,12 @@ class EztUnitTest(unittest.TestCase):
         self.bar = bar
     o = _BlahBlah('freedom', 'slavery')
     d = self._runTemplate('[X.foo] = [X.bar]', {'X': o})
-    self.assertEquals('freedom = slavery', d)
+    self.assertEqual('freedom = slavery', d)
 
   def testFor(self):
     o = [1, 4, 9, 16, 25]
     d = self._runTemplate('[for X][X] [end]', {'X': o})
-    self.assertEquals('1 4 9 16 25 ', d)
+    self.assertEqual('1 4 9 16 25 ', d)
 
   def testIsAny(self):
     t = """
@@ -94,7 +107,7 @@ class EztUnitTest(unittest.TestCase):
 [if-any O1 O4]O1O4[else]!O1O4[end]
 """
     d = self._runTemplate(t, {'O1': [], 'O2': None, 'O3': 0, 'O4': 'hi'})
-    self.assertEquals('\n!O1\n!O2\nO3\nO4\n!O1O2\nO1O4\n', d)
+    self.assertEqual('\n!O1\n!O2\nO3\nO4\n!O1O2\nO1O4\n', d)
 
   def testIfIndex(self):
     o = [1, 4, 9, 16, 25]
@@ -109,17 +122,17 @@ class EztUnitTest(unittest.TestCase):
 25 !FIRST LAST !ODD EVEN !THREE
 """
     d = self._runTemplate(t, {'X': o})
-    self.assertEquals(x, d)
+    self.assertEqual(x, d)
 
   def testIsVarLiteral(self):
     t = '[is WAR "peace"]yes[else]no[end][is FREEDOM "slavery"]yes[else]no[end]'
     d = self._runTemplate(t, {'WAR': 'war', 'FREEDOM': 'slavery'})
-    self.assertEquals('noyes', d)
+    self.assertEqual('noyes', d)
 
   def testIsLiteralVar(self):
     t = '[is "peace" WAR]yes[else]no[end][is "slavery" FREEDOM]yes[else]no[end]'
     d = self._runTemplate(t, {'WAR': 'peace', 'FREEDOM': 'freedom'})
-    self.assertEquals('yesno', d)
+    self.assertEqual('yesno', d)
 
   def testIsVarVar(self):
     t = '[is PEACE WAR]yes[else]no[end][is SLAVERY FREEDOM]yes[else]no[end]'
@@ -130,112 +143,112 @@ class EztUnitTest(unittest.TestCase):
       'SLAVERY': 'slavery',
     }
     d = self._runTemplate(t, v)
-    self.assertEquals('nono', d)
+    self.assertEqual('nono', d)
 
   def testIsLiteralLiteral(self):
     t = '[is "yes" "yes"]yes[else]no[end][is "no" "no"]maybe[else]sure![end]'
     d = self._runTemplate(t, {})
-    self.assertEquals('yesmaybe', d)
+    self.assertEqual('yesmaybe', d)
 
   def testSubst(self):
     d = self._runTemplate('["%2%% of 10 does not %0 %1!" A B C]',
         {'A': 'equal', 'B': '12345', 'C': 56789})
-    self.assertEquals('56789% of 10 does not equal 12345!', d)
+    self.assertEqual('56789% of 10 does not equal 12345!', d)
 
   def testSubstVarFormat(self):
     d = self._runTemplate('[FMT A B C]',
         {'A': 'equal', 'B': '12345', 'C': 56789,
          'FMT': '%2%% of 10 does not %0 %1!'})
-    self.assertEquals('56789% of 10 does not equal 12345!', d)
+    self.assertEqual('56789% of 10 does not equal 12345!', d)
 
   def testFormatNotLiteral(self):
     d = self._runTemplate('[D A B C]',
         {'A': 'equal', 'B': '12345', 'C': 56789,
          'D': '%2%% of 10 still does not %0 %1!'})
-    self.assertEquals('56789% of 10 still does not equal 12345!', d)
+    self.assertEqual('56789% of 10 still does not equal 12345!', d)
 
   def testInclude(self):
     d = self._runTemplateFile('include01.ezt',
                               {'MUCH': 'much', 'LITTLE': 'little'})
-    self.assertEquals('not much here, but\nthis file contains little\n\n', d)
+    self.assertEqual('not much here, but\nthis file contains little\n\n', d)
 
   def testIncludeNonLiteral(self):
     d = self._runTemplateFile('include03.ezt',
                               {'WHICH': 'include02.ezt', 'LITTLE': 'much'})
-    self.assertEquals('ha ha ha this file contains much\n ha ha ha\n', d)
+    self.assertEqual('ha ha ha this file contains much\n ha ha ha\n', d)
 
   def testInsert(self):
     d = self._runTemplateFile('insert01.ezt',
                               {'HERE': 'here'})
-    self.assertEquals('food goes in [HERE]\n\n', d)
+    self.assertEqual('food goes in [HERE]\n\n', d)
 
   def testInsertNonLiteral(self):
     d = self._runTemplateFile('insert03.ezt',
                               {'WHICH': 'insert02.ezt', 'HERE': 'here'})
-    self.assertEquals('[HERE]\n is the place\n', d)
+    self.assertEqual('[HERE]\n is the place\n', d)
 
   def testDefineSimple(self):
     d = self._runTemplate('[define RED]blue[end]RED = [RED]', {})
-    self.assertEquals('RED = blue', d)
+    self.assertEqual('RED = blue', d)
 
   def testDefineUnicode(self):
     d = self._runTemplate(u'[define HEART]♥[end]HEART = [HEART]', {})
-    self.assertEquals(u'HEART = ♥', d)
+    self.assertEqual(u'HEART = ♥', d)
 
   def testExceptionOnMissingVar(self):
     try:
       self._runTemplate('\n\n[GREEN]\n[RED]\n', {'GREEN': 'green'})
-    except ezt.UnknownReference, e:
-      self.assertEquals(e.line_number, 4)
+    except ezt.UnknownReference as e:
+      self.assertEqual(e.line_number, 4)
     else:
       self.fail('ezt.UnknownReference not raised')
 
   def testReplacementEscapeHTML(self):
     d = self._runTemplate('test [X].', {'X': '<>\'\"&'}, ezt.FORMAT_HTML)
-    self.assertEquals('test &lt;&gt;&#39;&quot;&amp;.', d)
+    self.assertEqual('test &lt;&gt;&#39;&quot;&amp;.', d)
 
   def testReplacementEscapeJS(self):
     d = self._runTemplate('test [X].', {'X': u'<>\'\"&\u2029'},
                           ezt.FORMAT_JS)
-    self.assertEquals('test \\x3c\\x3e\\x27\\x22\\x26\\u2029.', d)
+    self.assertEqual('test \\x3c\\x3e\\x27\\x22\\x26\\u2029.', d)
 
   def testReplacementEscapeURL(self):
     d = self._runTemplate('test [X].', {'X': u'<>\'\"&% \u2029'},
                           ezt.FORMAT_URL)
-    self.assertEquals('test %3C%3E%27%22%26%25+%E2%80%A9.', d)
+    self.assertEqual('test %3C%3E%27%22%26%25+%E2%80%A9.', d)
 
   def testFormat(self):
     d = self._runTemplate('[format "html"][X][end][format "js"][X][end]',
                           {'X': '<>\'\"&'})
-    self.assertEquals(r'&lt;&gt;&#39;&quot;&amp;\x3c\x3e\x27\x22\x26', d)
+    self.assertEqual(r'&lt;&gt;&#39;&quot;&amp;\x3c\x3e\x27\x22\x26', d)
 
   def testFormatNested(self):
     t = '[format "html,html"][X][end][format "html,js"][X][end]'
     d = self._runTemplate(t, {'X': '<>\'\"&'})
     htmlhtml_expected = r'&amp;lt;&amp;gt;&amp;#39;&amp;quot;&amp;amp;'
     htmljs_expected = r'\x26lt;\x26gt;\x26#39;\x26quot;\x26amp;'
-    self.assertEquals(htmlhtml_expected + htmljs_expected, d)
+    self.assertEqual(htmlhtml_expected + htmljs_expected, d)
 
   def testFormattedSubst(self):
     d = self._runTemplate('[format "html"]["%0" A][end]',
         {'A': '<b>hello</b>'})
-    self.assertEquals('&lt;b&gt;hello&lt;/b&gt;', d)
+    self.assertEqual('&lt;b&gt;hello&lt;/b&gt;', d)
 
   def testFormattedSubstUnicode(self):
     d = self._runTemplate(u'◄[format "html"]["%0" A][end]►',
         {'A': u'<b>♥</b>'})
-    self.assertEquals(u'◄&lt;b&gt;♥&lt;/b&gt;►', d)
+    self.assertEqual(u'◄&lt;b&gt;♥&lt;/b&gt;►', d)
 
   def testFormattedSubstVarFmt(self):
     d = self._runTemplate('[format "html"][FMT A][end]',
         {'A': '<b>hello</b>', 'FMT': '%0'})
-    self.assertEquals('&lt;b&gt;hello&lt;/b&gt;', d)
+    self.assertEqual('&lt;b&gt;hello&lt;/b&gt;', d)
 
   def testDoubleEndException(self):
     try:
       d = self._runTemplate('\n\n[if-any A]\n[A]\n[end]\n[end]\n', {'A': 'A'})
-    except ezt.UnmatchedEndError, e:
-      self.assertEquals(e.line_number, 6)
+    except ezt.UnmatchedEndError as e:
+      self.assertEqual(e.line_number, 6)
     else:
       self.fail('ezt.UnmatchedEndError not raised')
 
